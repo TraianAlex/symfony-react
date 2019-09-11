84a6b837df365127df3c309aa8bb756c5e8134be
Traian Alexandru <victor_traian@yahoo.com>
2019-09-11 15:18:45 -0400

    On branch master 	create a new component.... 	until immutable state 21 	modified:   assets/js/RepLog/RepLogApp.js 	new file:   assets/js/RepLog/RepLogCreator.js 	modified:   assets/js/RepLog/RepLogList.js 	modified:   assets/js/RepLog/RepLogs.js 	modified:   package.json 	modified:   webpack.config.js 	modified:   yarn.lock

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RepLogs from './RepLogs';
 import PropTypes from 'prop-types';
+import uuid from 'uuid/v4';
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -8,29 +9,57 @@ export default class RepLogApp extends Component {
 
         this.state = {
             highlightedRowId: null,
+            repLogs: [
+                { id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+                { id: uuid(), reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+                { id: uuid(), reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+            ]
         };
 
         this.handleRowClick = this.handleRowClick.bind(this);
+        this.handleAddRepLog = this.handleAddRepLog.bind(this);
     }
 
     handleRowClick(repLogId) {
         this.setState({highlightedRowId: repLogId});
     }
 
+    handleAddRepLog(itemLabel, reps) {
+        //const repLogs = this.state.repLogs;
+        const newRep = {
+            id: uuid(),
+            reps: reps,
+            itemLabel: itemLabel,
+            totalWeightLifted: Math.floor(Math.random() * 50)
+        };
+        //repLogs.push(newRep);
+        //const newRepLogs = [...this.state.repLogs, newRep];
+        //this.setState({repLogs: repLogs});
+        //this.setState({repLogs: newRepLogs});
+        this.setState(prevState => {
+            const newRepLogs = [...prevState.repLogs, newRep];
+            return {repLogs: newRepLogs};
+        });
+    }
+
     render() {
-        const { highlightedRowId } = this.state;
-        const { withHeart } = this.props;
+        // const { highlightedRowId, repLogs } = this.state;
+        // const { withHeart } = this.props;
 
         return (
             <RepLogs
-                withHeart={withHeart}
-                highlightedRowId={highlightedRowId}
+                // withHeart={withHeart}
+                // highlightedRowId={highlightedRowId}
+                // repLogs={repLogs}
+                {...this.props}
+                {...this.state}
                 onRowClick={this.handleRowClick}
+                onAddRepLog={this.handleAddRepLog}
             />
         )
     }
 }
 
 RepLogApp.propTypes = {
-    withHeart: PropTypes.bool
+    withHeart: PropTypes.bool,
 };
